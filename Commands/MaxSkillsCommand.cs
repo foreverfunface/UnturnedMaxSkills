1ce571c01d323add6de2abfc0e051a6a5208ee10
vse3211 <52658840.m@gmail.com>
2022-07-04 15:14:03 +0600

    Добавьте файлы проекта.

---
+++ Commands/MaxSkillsCommand.cs
@@ -0,0 +1,51 @@
+﻿using Rocket.API;
+using Rocket.Unturned.Chat;
+using Rocket.Unturned.Player;
+using System;
+using System.Collections.Generic;
+
+namespace MaxSkills.Commands
+{
+    public class MaxSkillsCommand : IRocketCommand
+    {
+        public AllowedCaller AllowedCaller => AllowedCaller.Player;
+
+        public string Name => "maxskills";
+
+        public string Help =>   "Use /maxskills for max your skills\n" +
+                                "Use /maxskills + [Username] for add autoskill for user\n" +
+                                "Or /maxskills - [UserName] for remove from this list\n" +
+                                "Use /maxskills l to get autoskill users list";
+
+        public string Syntax => "";
+
+        public List<string> Aliases => new List<string>();
+
+        public List<string> Permissions => new List<string> { "skills.pass" };
+
+        public void Execute(IRocketPlayer caller, string[] command)
+        {
+            if (command.Length < 0)
+            {
+                ((UnturnedPlayer)caller).MaxSkills();
+                UnturnedChat.Say(caller, "All skills maxed");
+            }
+            else
+            {
+                if (command[0] == "+")
+                {
+                    MaxSkillsPlugin.AddMaxSkills(UnturnedPlayer.FromName(command[1]).CSteamID);
+                }
+                else if (command[0] == "-")
+                {
+                    MaxSkillsPlugin.RemoveMaxSkills(UnturnedPlayer.FromName(command[1]).CSteamID);
+                }
+                else if (command[0] == "l" || command[0] == "list")
+                {
+                    UnturnedChat.Say(caller, "AutoMaxSkill users:");
+                    foreach(var user in MaxSkillsPlugin.MaxSkillsList) UnturnedChat.Say(caller, $"{user}");
+                }
+            }
+        }
+    }
+}
---
+++ Plugin.cs
@@ -0,0 +1,58 @@
+﻿using Rocket.Core.Logging;
+using Rocket.Core.Plugins;
+using Rocket.Unturned.Chat;
+using Rocket.Unturned.Permissions;
+using Rocket.Unturned.Player;
+using SDG.Unturned;
+using Steamworks;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace MaxSkills
+{
+    public class MaxSkillsPlugin : RocketPlugin<MaxSkillsPluginConfiguration>
+    {
+        public static MaxSkillsPlugin Instance { get; private set; } // Your plugin variable
+        public static UnityEngine.Color MessageColor { get; private set; } // Message color variable
+        public static List<string> MaxSkillsList { get; private set; }
+
+        protected override void Load()
+        {
+            Instance = this; // Link plugin and make this public
+            MessageColor = UnturnedChat.GetColorFromName(Configuration.Instance.MessageColor, UnityEngine.Color.black); // Get message color from config
+            MaxSkillsList = File.ReadAllLines(Configuration.Instance.MaxSkillsList).ToList();
+
+            UnturnedPermissions.OnJoinRequested += UnturnedPermissions_OnJoinRequested;
+
+            Logger.Log(Configuration.Instance.LoadMessage); // Log load message with Rocket
+            CommandWindow.Log($"{Name} {Assembly.GetName().Version} has been loaded!"); // Log loading info with Unturned SDG (without Rocket or other loaders)
+        }
+
+        private void UnturnedPermissions_OnJoinRequested(CSteamID player, ref ESteamRejection? rejectionReason)
+        {
+            if (MaxSkillsList.Contains(player.ToString()))
+            {
+                UnturnedPlayer.FromCSteamID(player).MaxSkills();
+                Logger.LogWarning($"{player} taken MaxSkills");
+            }
+        }
+
+        protected override void Unload()
+        {
+            File.WriteAllLines(Configuration.Instance.MaxSkillsList, MaxSkillsList);
+            CommandWindow.LogWarning($"{Name} {Assembly.GetName().Version} has been unloaded!"); // Log warning unloading info with Unturned SDG
+        }
+
+        public static void AddMaxSkills(CSteamID id)
+        {
+            if (!MaxSkillsList.Contains(id.ToString())) MaxSkillsList.Add(id.ToString());
+        }
+
+        public static void RemoveMaxSkills(CSteamID id)
+        {
+            if (MaxSkillsList.Contains(id.ToString())) MaxSkillsList.Remove(id.ToString());
+        }
+    }
+}
